c28b444981e823add04980c377ecfc52839ef7b6
Mark Andrew Acosta Jamito <64141186+Mario10100MC@users.noreply.github.com>
2021-10-14 02:05:12 +0800

    Update To Install.sh
    tweaked a bit
    -Tested With KDE Plasma 5 on Arch Linux

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-#TODO
+#!/usr/bin/bash
+git clone https://github.com/Pear-Project/pearOS-Default-Launchpad
+cd pearOS-Default-Launchpad
+/usr/bin/mkdir -p ~/.local/share/plasma/plasmoids/pearOSLaunchpad
+/usr/bin/cp -r ./* ~/.local/share/plasma/plasmoids/pearOSLaunchpad/
